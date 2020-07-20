bd4846121ed9ec83efeb0bf246e0732659bd9d99
Brimes7 <48000565+Brimes7@users.noreply.github.com>
2020-07-19 22:06:28 -0500

    EXAMPLE 2 COMPLETE ALL TESTS PASS W COMMENTARY

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -9,6 +9,18 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    #Create the hash table
+    cachefly = {}
+    #Creating the route
+    route = [None] * length
+    #This check to see which flight is next on our list.
+    #if we hit the end early the next flight will be none
+    for t in tickets:
+        cachefly[t.source] = t.destination
+    next = cachefly["NONE"]
+    #checks the flights as well starting from 0 the end of route
+    for z in range(0, length):
+        route[z] = next
+        next = cachefly[next]
 
     return route
