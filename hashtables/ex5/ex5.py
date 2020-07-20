bcb1f1277322b7994401fc25a36d6b19ba99b206
Brimes7 <48000565+Brimes7@users.noreply.github.com>
2020-07-19 23:29:00 -0500

    EXAMPLE 5 FULLY FUNCTIONING. BOTH TESTS ARE ACCURATE

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -7,6 +7,32 @@ def finder(files, queries):
     YOUR CODE HERE
     """
     file_cache = {}
+    result = []
+
+    #need tp create an a path
+    for p in files:
+        #where will we find the file?
+        #NEED TO LOOK AFTER the "/"
+        #Checks one integer back to make sure there is a "/"
+        file = p.split('/')[-1]
+        #if we can actaully find file
+        #add file to the locations
+        if file in file_cache:
+            file_cache[file].append(p)
+        #if the file isn't known we need to create new files
+        else:
+            file_cache[file] = [p]
+    #Now we need to check if queries is in the files_cache
+    for q in queries:
+        #Checking for the query in the cached files
+        if q in file_cache:
+            #Creating results not the same as result
+            results = file_cache[q]
+            #This will collapse our lists into one list w double for loop
+            #p is the path in the query
+            for p in results:
+                #This will add the results to the path of FINAL Result
+                result.append(p)
 
     return result
 
